b163f92f983430b7c7e203a55f06ecfba08d52ad
Daniel Sperber <github.blurry@9ox.net>
2025-02-13 18:56:51 +0100

    feat: use ray_utilities logging handler

--- interpretable_ddts/__init__.py
+++ interpretable_ddts/__init__.py
@@ -1,3 +1,12 @@
+import logging
+
+from ray_utilities import utilities_handler
+
 from .ddt_setup import DDTSetup
+
+logger = logging.getLogger(__name__)
+if not logger.hasHandlers():
+    logger.setLevel(logging.INFO)
+    logger.addHandler(utilities_handler)
 
 __all__ = ["DDTSetup"]
--- interpretable_ddts/runfiles/rllib_setup.py
+++ interpretable_ddts/runfiles/rllib_setup.py
@@ -1,18 +1,15 @@
 from __future__ import annotations
 
-import logging
 from pprint import pprint
 from typing import TYPE_CHECKING
 
 # Import comet before torch to allow monkey patching.
 from ray_utilities import run_tune  # fmt: skip
 
-from interpretable_ddts import DDTSetup
+from interpretable_ddts import DDTSetup, logger
 
 if TYPE_CHECKING:
     from ray_utilities.typing import FunctionalTrainable
-
-logger = logging.getLogger(__name__)
 
 
 def test_mode_func(trainable: FunctionalTrainable, setup: DDTSetup):
@@ -30,3 +27,4 @@ if __name__ == "__main__":
     results = run_tune(setup, test_mode_func)
     if setup.args.test:
         pprint(results)
+        logger.info("End of test run")
